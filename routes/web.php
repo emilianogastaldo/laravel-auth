ead9fc6dd75557b36070f793b86261a448a8324e
emilianogastaldo <emiliano0303@gmail.com>
2024-03-19 18:14:17 +0100

    create le rotte e spostato il controller

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,8 @@
 
 use App\Http\Controllers\Admin\HomeController as AdminHomeController;
 use App\Http\Controllers\Guest\HomeController as GuestHomeControler;
+use App\Http\Controllers\Admin\ProjectController;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,17 +19,25 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', GuestHomeControler::class)->name('guest.home');
 
-Route::get('/admin', AdminHomeController::class)->middleware(['auth', 'verified'])->name('admin.home');
 
-Route::get('/projects', [ProjectController::class, 'index'])->name('projects.index');
-Route::get('/projects/create', [ProjectController::class, 'create'])->name('projects.create');
-Route::get('/projects/{project}', [ProjectController::class, 'show'])->name('projects.show');
-Route::post('/projects', [ProjectController::class, 'store'])->name('projects.store');
-Route::get('/projects/{post}/edit', [ProjectController::class, 'edit'])->name('projects.edit');
-Route::put('/projects/{post}', [ProjectController::class, 'update'])->name('projects.update');
-Route::delete('/projects/{post}', [ProjectController::class, 'destroy'])->name('projects.destroy');
+Route::prefix('/admin')->name('admin.')->middleware('auth')->group(function () {
+    // Rotta home admin
+    Route::get('', AdminHomeController::class)->middleware('auth')->name('home');
+
+    // Rotta projects admin
+    Route::resource('projects', ProjectController::class);
+    // Che è come fare queste 7 righe:
+    // Route::get('/projects', [ProjectController::class, 'index'])->name('projects.index');
+    // Route::get('/projects/create', [ProjectController::class, 'create'])->name('projects.create');
+    // Route::get('/projects/{project}', [ProjectController::class, 'show'])->name('projects.show');
+    // Route::post('/projects', [ProjectController::class, 'store'])->name('projects.store');
+    // Route::get('/projects/{post}/edit', [ProjectController::class, 'edit'])->name('projects.edit');
+    // Route::put('/projects/{post}', [ProjectController::class, 'update'])->name('projects.update');
+    // Route::delete('/projects/{post}', [ProjectController::class, 'destroy'])->name('projects.destroy');
+});
 
 
+// Rotte profilo
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
