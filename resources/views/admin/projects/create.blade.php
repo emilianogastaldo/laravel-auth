3eba5e02d58dfafd1f85650ee50f93854c974dbc
emilianogastaldo <emiliano0303@gmail.com>
2024-03-20 19:10:55 +0100

    aggiunta preview

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -5,3 +5,16 @@
 @section('content')
 @include('includes.projects.form')
 @endsection
+
+@section('scripts')
+<script>
+    const placeholder = 'https://marcolanci.it/boolean/assets/placeholder.png';
+    const input = document.getElementById('image');
+    const preview = document.getElementById('preview');
+
+    input.addEventListener('input', () =>{
+        preview.src = input.value || placeholder;
+    })
+</script>
+
+@endsection
