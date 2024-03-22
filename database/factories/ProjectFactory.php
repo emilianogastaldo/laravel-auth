c89199b5917936c43af22f070e743ae17f5aba5e
emilianogastaldo <emiliano0303@gmail.com>
2024-03-22 16:15:17 +0100

    aggiornata la factory

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 /**
@@ -18,11 +19,14 @@ class ProjectFactory extends Factory
     public function definition(): array
     {
         $title = fake()->text(20);
+        $slug = Str::slug($title);
+        $image = fake()->image(null, 300, 300);
+        $img_url = Storage::putFileAs('project_images', $image, "$slug.png");
         return [
             'title' => $title,
-            'slug' => Str::slug($title),
+            'slug' => $slug,
             'content' => fake()->paragraph(15, true),
-            'image' => fake()->imageUrl(250, 250, true),
+            'image' => $img_url,
         ];
     }
 }
