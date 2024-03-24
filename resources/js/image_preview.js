f997a3cdba5e8a3f2a9987401f30016b6cbbd3dc
emilianogastaldo <emiliano0303@gmail.com>
2024-03-25 00:09:10 +0100

    pulizia blob

--- resources/js/image_preview.js
+++ resources/js/image_preview.js
@@ -12,13 +12,15 @@ const previewField = document.getElementById('preview');
 //     preview.src = input.value || placeholder;
 // })
 
+let blobUrl;
+
 imageFiled.addEventListener('change', () => {
     // controllo se ho il file
     if (imageFiled.files && imageFiled.files[0]) {
         // prendo il file
         const file = imageFiled.files[0];
         //  URL temporaneo
-        const blobUrl = URL.createObjectURL(file);
+        blobUrl = URL.createObjectURL(file);
 
         previewField.src = blobUrl;
     }
@@ -26,4 +28,9 @@ imageFiled.addEventListener('change', () => {
         previewField.src = placeholder;
     }
 
-})
+});
+
+// I blob sono pesanti, quindi quando lascio la pagina devo cancellarlo
+window.addEventListener('beforeunload', () => {
+    if (blobUrl) URL.revokeObjectURL(blobUrl);
+});
