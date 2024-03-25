0ab70a884866aa908b630d8a3cdc6cdc53e957c3
emilianogastaldo <emiliano0303@gmail.com>
2024-03-25 01:22:27 +0100

    creata home guest

--- app/Http/Controllers/Guest/HomeController.php
+++ app/Http/Controllers/Guest/HomeController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function __invoke()
     {
-        return view('guest.home');
+        $projects = Project::orderByDesc('created_at')->paginate(5);
+        return view('guest.home', compact('projects'));
     }
 }
--- app/Http/Controllers/Guest/ProjectController.php
+++ app/Http/Controllers/Guest/ProjectController.php
@@ -8,8 +8,11 @@ use App\Models\Project;
 
 class ProjectController extends Controller
 {
-    public function show(Project $project)
+    public function show(string $slug)
     {
+        $project = Project::whereSlug($slug)->first();
+
+        if (!$project) abort(404);
         return view('guest.projects.show', compact('project'));
     }
 }
